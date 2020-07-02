f5d0d61f3fe1a367a8cf956197cecc8f01f45423
Alexander Belinsky <squarrelhouse.ai@gmail.com>
2020-07-02 22:00:50 +0300

    last minute change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -8,13 +8,16 @@ it('response with details about current user', async () => {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
-    .expect(200);
+    .expect(400);
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
 
 it('responds with null if not authenticated', async () => {
-  const response = await request(app).get('/api/users/currentuser').send().expect(200);
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
 
   expect(response.body.currentUser).toEqual(null);
 });
